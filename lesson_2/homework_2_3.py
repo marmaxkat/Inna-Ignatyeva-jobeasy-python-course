a0ad478e3718f93b722f15f4f5745aa1a8d9c2a8
marmaxkat <marmaxkat@gmail.com>
2020-12-12 18:31:13 -0800

    Inna_Ignatyeva_homework_2_3.py

--- lesson_2/homework_2_3.py
+++ lesson_2/homework_2_3.py
@@ -3,5 +3,7 @@
 # !important you should save only number to result_temperature. Formula (32°F − 32) × 5/9 = 0°C
 
 # type your code here
-temperature_fahrenheit = None
-result_temperature = None
+temperature_fahrenheit = int(input('Enter the temperature in Fahrenheit: '))
+celsius = (temperature_fahrenheit - 32)*5/9
+result_temperature = round(celsius,1)
+print(result_temperature)
